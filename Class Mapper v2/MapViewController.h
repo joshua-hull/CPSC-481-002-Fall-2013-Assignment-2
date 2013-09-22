f88a5540c14d7d6671f37b0b3c456bfb5b1c7990
Joshua Hull <hyperspace290@gmail.com>
2013-09-21 20:44:15 -0400

    Map View working. Also added gpx location file to allow simulating location on campus.

--- Class Mapper v2/MapViewController.h
+++ Class Mapper v2/MapViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface MapViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
 
 @end
--- Class Mapper v2/MapViewController.m
+++ Class Mapper v2/MapViewController.m
@@ -13,11 +13,43 @@
 @end
 
 @implementation MapViewController
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CLLocationCoordinate2D zoom;
+    zoom.latitude = 34.6783;
+    zoom.longitude = -82.8392;
+    
+    CLLocationCoordinate2D cooperLongLat;
+    cooperLongLat.latitude = 34.6767716;
+    cooperLongLat.longitude = -82.836532;
+    
+    CLLocationCoordinate2D tillmanLongLat;
+    tillmanLongLat.latitude = 34.6801049;
+    tillmanLongLat.longitude = -82.8373654;
+    
+    MKCoordinateRegion view = MKCoordinateRegionMakeWithDistance(zoom, 1000,1000);
+    
+    MKPointAnnotation *cooperLibrary = [[MKPointAnnotation alloc] init];
+    cooperLibrary.coordinate = cooperLongLat;
+    cooperLibrary.title = @"Cooper Library";
+    
+    MKPointAnnotation *tillmanHall = [[MKPointAnnotation alloc] init];
+    tillmanHall.coordinate = tillmanLongLat;
+    tillmanHall.title = @"Tillman Hall";
+    
+    [mapView addAnnotation:cooperLibrary];
+    [mapView addAnnotation:tillmanHall];
+    
+    
+    [mapView setRegion:view animated:YES];
+    
+    [mapVi]
+    
 }
 
 - (void)didReceiveMemoryWarning
